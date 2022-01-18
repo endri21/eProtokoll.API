dbe86e41494db43c6ca90ccb77d79c4041ee5e20
endri21 <41783661+endri21@users.noreply.github.com>
2022-01-18 22:52:58 +0100

    notifications

---
+++ swagger/Controllers/NotificationsController.cs
@@ -0,0 +1,23 @@
+﻿using eProtokollCore.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace swagger.Controllers
+{
+    public class NotificationsController : ApiController
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationsController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        [HttpGet]
+        [Route(nameof(GetNotifications))]
+        public async Task<IActionResult> GetNotifications()
+        {
+            return Ok(await _notificationService.GetUnReadNotifications());
+        }
+    }
+}
